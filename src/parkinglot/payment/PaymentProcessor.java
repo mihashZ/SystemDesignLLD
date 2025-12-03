2e17bd7f678e40e0d9fbfd398b28931a2de1a9ed
mihash <mihashkh@gmail.com>
2025-12-03 16:05:55 +0530

    small changes

--- src/parkinglot/payment/PaymentProcessor.java
+++ src/parkinglot/payment/PaymentProcessor.java
@@ -1,10 +1,10 @@
 package parkinglot.payment;
 
-public class Payment {
+public class PaymentProcessor {
     private double amount;
     private PaymentStrategy paymentStrategy; // ParkingLot.PaymentStrategyPattern.Payment strategy interface
     // Constructor to initialize the payment amount and payment strategy
-    public Payment(double amount, PaymentStrategy paymentStrategy) {
+    public PaymentProcessor(double amount, PaymentStrategy paymentStrategy) {
         this.amount = amount;
         this.paymentStrategy = paymentStrategy;
     }
